d18c2728a0ed3ebc2e9f002385098446f1eec0b8
Bruno Hildenbrand <hd9x@outlook.com>
2020-02-27 15:40:45 -0700

    More updates

---
+++ Program.cs
@@ -0,0 +1,49 @@
+// **********************************************
+// Fore more information, visit: 
+// https://blog.hildenco.com/2020/02/migrating-nservicebus-backends-to-azure.html
+// **********************************************
+
+// Related docs:
+//   - NSB WebJobs:   https://docs.particular.net/samples/azure/webjob-host/
+//   - Azure WebJobs: https://docs.microsoft.com/en-us/azure/app-service/webjobs-sdk-get-started
+
+
+// Example class to show how to run NServiceBus endpoints
+// as Azure WebJobs
+public class Program
+{
+    static void Main()
+    {
+        try
+        {
+            var ec = new EndpointConfiguration("MyApp.NServiceBus.Backend");
+            new EndpointConfig().Customize(ec);
+            Task.Run(async () => await StartAsync(ec)).Wait();
+        }
+        catch (AggregateException e)
+        {
+            Console.Error.WriteLine(e);
+        }
+    }
+
+    private static async Task StartAsync(EndpointConfiguration ec)
+    {
+        IEndpointInstance endpoint = null;
+        endpoint = await Endpoint.Start(ec).ConfigureAwait(false);
+
+        #if DEBUG
+        Console.ReadLine();
+        #else
+        var cs = ConfigurationManager.ConnectionStrings["MyApp.NServiceBus.Backend.ConnectionString"].ConnectionString;
+        var builder = new HostBuilder();
+        var host = builder.Build();
+        using (host)
+        {
+            host.Run();
+        };
+        #endif
+
+        // shuts down NSB to properly release all acquired resources
+        await endpoint.Stop().ConfigureAwait(false);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# nservicebus-webjobs
-Repo to describe how to run NServiceBus backends on Azure WebJobs
+# NServiceBus Webjobs
+Repo to describe how to run [NServiceBus](https://particular.net/nservicebus) backends as 
+[Azure WebJobs](https://docs.microsoft.com/en-us/azure/app-service/webjobs-create).
+
+## Complete Information
+The complete use case is described on my blog at: [Migrating NServiceBus
+backends to Azure WebJobs](https://blog.hildenco.com/2020/02/migrating-nservicebus-backends-to-azure.html).
+
+## Source Code
+I added an example on how to intialize the enpoint asynchronously at
+`Program.cs`. 
+
+Why name the class `Program.cs`? Because a WebJob can be essentially a simple console
+application. Keeping the standard name for .NET console applications just made
+sense.
+
+## Deployment
+Deployment is also addressed on the post. However, if you're using
+[NuGet](https://nuget.org) and bundling your deployment in a NuGet package by
+using `Nuspec` files, a simplified version using can be found at `app.nuspec` file.
+
+## Thanks
+Thanks and don't forget to visit my blog at
+[blog.hildenco.com](https://blog.hildenco.com)
+
+## License
+MIT
+
+
